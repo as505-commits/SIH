524fb36729c4931e6697c566fc9baf95b7a55d55
anushka140118-gif <anushka140118@gmail.com>
2026-09-06 01:52:24 +0530

    Update RiskFactorService.java

--- backend/src/main/java/com/sih/backend/service/RiskFactorService.java
+++ backend/src/main/java/com/sih/backend/service/RiskFactorService.java
@@ -2,36 +2,29 @@ package com.sih.backend.service;
 
 import com.sih.backend.model.PredictionRequest;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class RiskFactorService {
-
     public List<String> getRiskFactors(PredictionRequest request) {
-
         List<String> riskFactors = new ArrayList<>();
-
         if (request.getSleepHours() < 6) {
             riskFactors.add("Low sleep hours");
         }
 
         if (request.getWorkPressureLevel() != null &&
                 request.getWorkPressureLevel().equalsIgnoreCase("High")) {
-
             riskFactors.add("High work pressure");
         }
 
         if (request.getWorkLifeBalance() != null &&
                 request.getWorkLifeBalance().equalsIgnoreCase("Low")) {
-
             riskFactors.add("Poor work-life balance");
         }
 
         if (request.getTrainingOpportunities() != null &&
                 request.getTrainingOpportunities().equalsIgnoreCase("No")) {
-
             riskFactors.add("Limited training opportunities");
         }
 
